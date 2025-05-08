e058a60d9fbcc0fbecad90285b75d866f8260746
Matt <matt@signalblue.net>
2025-05-08 16:41:37 -0700

    Try to build without example dir

--- example/src/auth0-configuration.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  clientId: 'your-client-id',
-  domain: 'your-domain',
-  clientIdSecondScreen: 'your-client-id',
-  domainSecondScreen: 'your-domain',
-};
-
-export default config;
